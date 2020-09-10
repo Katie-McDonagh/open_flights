ca9e9025de42db071e5880e98855a6d6fa1a4802
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-09-10 21:54:18 +0100

    refactor Airplines.js

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -33,12 +33,8 @@ const Airlines = () => {
   const [airlines, setAirLines] = useState([])
 
   useEffect(() => {
-    //get all airlines from API
-    //update airlines on our site
     Axios.get('./api/v1/airlines.json')
-    .then( resp => {
-      setAirLines(resp.data.data)
-    } )
+    .then( resp => setAirLines(resp.data.data) )
     .catch( resp => console.log(resp) )
   }, [airlines.length])
 
