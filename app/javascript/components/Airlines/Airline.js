855ea2cf19d958cc5dee8382d106d7779b705be8
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-09-10 21:42:38 +0100

    style conponent - Card

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+`
+const AirlineLogo = styled.div``
+const AirlineName = styled.div``
+const LinkWrapper = styled.div``
+
+
 const Airline = () => {
   return (
-    <div className='card'>
+    <Card>
       <div className='airline-logo'>
         <img src={props.attributes.mage_url} alt={props.attributes.name}/>
       </div>
       <div className='airline-name'>{props.attributes.name}</div>
       <div className='airline-score'>{props.attributes.avg_score}</div>
       <div className='airline-link'>
-        <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link>
+        <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link >
       </div>
-    </div>
+    </Card>
   )
 }
 
