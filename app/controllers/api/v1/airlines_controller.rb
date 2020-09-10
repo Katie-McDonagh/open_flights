00fca8ba45737575b8effc7a63b5311494042bf9
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-09-10 18:54:30 +0100

    add airline controller show

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -6,6 +6,11 @@ module Api
 
         render json: AirlineSerializer.new(airlines).serialized_json
       end
+      def show
+        airline = Airline.find_by(slug: params[:slug])
+
+        render json: AirlineSerializer.new(airline).serialized_json
+      end
     end
   end
 end
