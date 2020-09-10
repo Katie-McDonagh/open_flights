e95e3676e93ae0f75454e0ac7f22b6ab0cef043e
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-09-10 20:35:56 +0100

    correct routes to point to correct components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Airlines from './Airlines/Airlines'
+import Airline from './Airline/Airline'
+
 
 const App = () => {
   return (
