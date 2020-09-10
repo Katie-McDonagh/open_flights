99d2fce19483c1241cee5a1dbc8198e4b688f9ac
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-09-10 20:09:36 +0100

    add App.js

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: propTypes.string
-}
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import App from '../src/components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name='React' />,
+    <Router>
+      <Route path="/" component={App}/>
+    </Router>,
     document.body.appendChild(document.createElement('div')),
   )
 })
