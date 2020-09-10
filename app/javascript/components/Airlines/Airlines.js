4c7fdbb1d680dae589f58204da9dce692bf195bc
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-09-10 20:45:19 +0100

    display Axois test

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
 
 const Airlines = () => {
   const [airlines, setAirLines] = useState([])
@@ -6,8 +7,10 @@ const Airlines = () => {
   useEffect(() => {
     //get all airlines from API
     //update airlines on our site
-
-  })
+    Axios.get('./api/v1/airlines.json')
+    .then( resp => console.log(resp) )
+    .catch( resp => console.log(resp) )
+  }, [airlines.length])
 
   return (
   <div>This is the Airlines#index for our app</div>
