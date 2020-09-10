cf813936f6a68f689f228adb929a35574bcf96ac
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-09-10 22:12:00 +0100

    use Axios for Airline.js

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,4 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const Airline = (props) => {
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState({})
+
+  useEffect(()=> {
+    const slug = props.match.params.slug
+    const url = `/api/v1/airlines/${slug}`
+    // This uses the v2 api (graphql) as of 05/09/2020.
+    // For the v1 api endpoint use: axios.get('/api/v1/airlines/:slug')
+    axios.get(url)
+    .then( resp => console.log(resp))
+    .catch( resp => console.log(resp))
+
+  }, [])
 
 const Airline = () => {
   return <div>This is the Airline#show for our app</div>
